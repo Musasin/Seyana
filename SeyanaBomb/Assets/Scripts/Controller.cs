8ad4b4d5e68142691df86ce61768b835f8c4058c
Musasin <c0112051b9@edu.teu.ac.jp>
2020-10-06 21:28:49 +0900

    TitleScene追加

---
+++ SeyanaBomb/Assets/Scripts/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+ 
+            GameObject clickedGameObject = null;
+ 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+                if (clickedGameObject.tag == "Player")
+                {
+                    Seyana script = clickedGameObject.GetComponent<Seyana>();
+                    if (script != null)
+                    {
+                        script.Grip();
+                    }
+                }
+            }
+ 
+            Debug.Log(clickedGameObject);
+        }
+    }
+}
---
+++ SeyanaBomb/Assets/Scripts/Talk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Talk : MonoBehaviour
+{
+    public GameObject selifLeft;
+    public GameObject selifRight;
+    GameObject nowSelifObject;
+    int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.Instance.PlayBGM("Title");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            switch (index)
+            {
+                case 0:
+                    index++;
+                    nowSelifObject = Instantiate(selifLeft, transform);
+                    nowSelifObject.GetComponentInChildren<Text>().text = "セヤナー爆弾が\nできたよお姉ちゃん！";
+                    break;
+                case 1:
+                    index++;
+                    Destroy(nowSelifObject);
+                    nowSelifObject = Instantiate(selifRight, transform);
+                    nowSelifObject.GetComponentInChildren<Text>().text = "セヤナー爆弾？！";
+                    break;
+            }
+        }
+
+    }
+}
