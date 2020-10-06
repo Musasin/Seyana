55eb94258b91f4be2c2e688215362290f5c62361
Musasin <c0112051b9@edu.teu.ac.jp>
2020-10-06 19:52:42 +0900

    掴む機能追加

---
+++ SeyanaBomb/Assets/Seyana.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seyana : MonoBehaviour
+{
+    bool isGrip;
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrip)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                isGrip = false;
+            } 
+            else
+            {
+                var screenPos = Input.mousePosition;
+                screenPos.z = Mathf.Abs(mainCamera.transform.position.z);
+                transform.position = mainCamera.ScreenToWorldPoint(screenPos);
+            }
+
+        }
+    }
+
+    public void Grip()
+    {
+        isGrip = true;
+    }
+}
