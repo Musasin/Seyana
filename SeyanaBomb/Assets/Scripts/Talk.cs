0383f60f120a9a86a069f291481f4982d68b2504
Musasin <c0112051b9@edu.teu.ac.jp>
2020-10-13 01:43:50 +0900

    fix

--- SeyanaBomb/Assets/Scripts/Talk.cs
+++ SeyanaBomb/Assets/Scripts/Talk.cs
@@ -384,6 +384,7 @@ public class Talk : MonoBehaviour
                 } else
                 {
                     SceneManager.LoadScene("EndingScene");
+                    return;
                 }
             }
         }
